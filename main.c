11f11eb5e486219d6dc8384fac38ea572d326f41
22103324 <22103324@mail.jiit.ac.in>
2024-07-31 17:00:57 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 #define MAX_WORDS 100
 #define MAX_WORD_LEN 50
 
-// Function to convert a string to lowercase
 void to_lowercase(char* str) {
     for (int i = 0; str[i]; i++) {
         str[i] = tolower(str[i]);
@@ -18,16 +17,13 @@ int main() {
     int word_count[MAX_WORDS] = {0};
     int num_words = 0;
 
-    // Get paragraph input from the user
     printf("Enter a paragraph: ");
     fgets(paragraph, sizeof(paragraph), stdin);
 
-    // Tokenize the paragraph into words
     char* token = strtok(paragraph, " .,;:!\n");
     while (token != NULL) {
         to_lowercase(token);
 
-        // Check if the word already exists in the words array
         int found = 0;
         for (int i = 0; i < num_words; i++) {
             if (strcmp(words[i], token) == 0) {
@@ -37,7 +33,6 @@ int main() {
             }
         }
 
-        // If the word is not found, add it to the words array
         if (!found) {
             strcpy(words[num_words], token);
             word_count[num_words]++;
@@ -47,7 +42,6 @@ int main() {
         token = strtok(NULL, " .,;:!\n");
     }
 
-    // Print the word frequencies
     printf("\nWord Frequencies:\n");
     for (int i = 0; i < num_words; i++) {
         printf("%s: %d\n", words[i], word_count[i]);
